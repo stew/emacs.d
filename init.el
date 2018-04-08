16cddb882a3fa5bfd8fea7eaefdc7b5dcf8e5c64
Stew O'Connor <stew@vireo.org>
2018-04-08 08:10:37 -0700

    stuff

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
 (require 'ob-tangle)
 (org-babel-load-file "~/.emacs.d/Stew.org")
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,44 +30,67 @@
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
-	("55d31108a7dc4a268a1432cd60a7558824223684afecefa6fae327212c40f8d3" default)))
+    ("55d31108a7dc4a268a1432cd60a7558824223684afecefa6fae327212c40f8d3" default)))
+ '(dired-dwim-target t)
  '(ensime-graphical-tooltips t)
  '(exec-path
    (quote
-	("~/.nix-profile/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin" "~/bin" "~/.cargo/bin")))
- '(flycheck-rust-library-path (quote ("/home/stew/.cargo/bin")))
+    ("~/.sdkman/candidates/sbt/current/bin" "~/.sdkman/candidates/java/current/bin" "~/.nix-profile/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin" "~/bin" "~/.cargo/bin")))
+ '(git-link-remote-alist
+   (quote
+    (("github" git-link-github)
+     ("bitbucket" git-link-bitbucket)
+     ("gitorious" git-link-gitorious)
+     ("gitlab" git-link-gitlab)
+     ("gh" git-link-github))))
  '(grep-find-ignored-directories
    (quote
-	("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".ensime_cache" "target")))
+    ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".ensime_cache" "target")))
+ '(haskell-ask-also-kill-buffers nil)
  '(haskell-font-lock-symbols t)
+ '(haskell-interactive-popup-errors nil)
  '(haskell-mode-hook
    (quote
-	(turn-on-eldoc-mode turn-on-haskell-decl-scan turn-on-haskell-doc turn-on-haskell-indentation)) t)
- '(idris-interpreter-path "/home/stew/.cabal/bin/idris")
- '(indent-tabs-mode t)
+    (turn-on-eldoc-mode turn-on-haskell-decl-scan turn-on-haskell-doc turn-on-haskell-indentation)))
+ '(haskell-process-type (quote stack-ghci))
+ '(indent-tabs-mode nil)
  '(ispell-program-name "aspell")
+ '(jdee-server-dir "~/src/jdee-server/target")
  '(line-move-visual nil)
  '(menu-bar-mode nil)
  '(nil nil t)
  '(notmuch-hello-thousands-separator ",")
  '(notmuch-saved-searches
    (quote
-	(("inbox" . "tag:inbox")
-	 ("unread" . "tag:unread")
-	 ("inbox+unread" . "tag:inbox and tag:unread")
-	 ("cody" . "from:cody")
-	 ("cody+unread" . "from:cody and tag:unread"))))
+    (("inbox" . "tag:inbox")
+     ("unread" . "tag:unread")
+     ("inbox+unread" . "tag:inbox and tag:unread")
+     ("cody" . "from:cody")
+     ("cody+unread" . "from:cody and tag:unread"))))
  '(notmuch-search-oldest-first nil)
  '(notmuch-show-all-multipart/alternative-parts nil)
+ '(nxml-child-indent 4)
+ '(org-agenda-custom-commands
+   (quote
+    (("n" "Agenda and all TODO's"
+      ((agenda "" nil)
+       (alltodo "" nil))
+      nil)
+     ("x" "stew's agenda" tags "NOW" nil))))
  '(org-babel-load-languages (quote ((sh . t) (awk . t) (emacs-lisp . t))))
  '(org-confirm-babel-evaluate nil)
  '(org-modules
    (quote
-	(org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-protocol org-rmail org-w3m org-drill org-git-link)))
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-protocol)))
  '(package-selected-packages
    (quote
-	(znc yaml-mode use-package twittering-mode smex scala-mode2 rust-mode paredit org-jira notmuch-labeler multiple-cursors markdown-mode magit key-chord json-mode intero idris-mode hydra helm-projectile helm-grepint helm-git-grep hcl-mode golint go-eldoc go-autocomplete git-link git-gutter flymake-rust ensime confluence avy autumn-light-theme ansible ace-jump-mode)))
+    (haskell-mode pretty-mode ivy ensime cider git-gutter+ jdee counsel znc yaml-mode use-package twittering-mode smex scala-mode2 rust-mode paredit org-jira multiple-cursors markdown-mode magit key-chord json-mode intero hydra helm-projectile helm-grepint helm-git-grep hcl-mode golint go-eldoc go-autocomplete flymake-rust confluence avy autumn-light-theme ansible ace-jump-mode)))
  '(python-indent-offset 2)
+ '(sbt:program-name
+   "/Users/stew.oconnor/.sdkman/candidates/sbt/current/bin/sbt")
+ '(scala-indent:add-space-for-scaladoc-asterisk nil)
+ '(scala-indent:align-parameters t)
+ '(scala-indent:use-javadoc-style t)
  '(send-mail-function (quote sendmail-send-it))
  '(show-paren-mode t)
  '(split-width-threshold 200)
@@ -74,6 +98,7 @@
  '(tab-width 4)
  '(tool-bar-mode nil)
  '(twittering-username "stewoconnor")
+ '(with-editor-emacsclient-executable "~/.nix-profile/bin/emacsclient")
  '(yas-snippet-dirs (quote ("~/.emacs.d/snippets"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
